55b213ceed0b55fce111c364c06af28d83bb26bd
talalhassan965 <taliiijan123@gmail.com>
2024-05-26 11:12:20 +0500

    create signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,29 +1,58 @@
-import User from '../models/user.modle.js'
-import bcrytpjs from 'bcryptjs'
-import { errorHandler } from '../util/error.js';
+import User from "../models/user.modle.js";
+import bcrytpjs from "bcryptjs";
+import { errorHandler } from "../util/error.js";
+import jwt from "jsonwebtoken";
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
+  }
 
+  const hashedPassword = bcrytpjs.hashSync(password, 10);
 
-export const signup = async (req, res, next) =>{
-    const {username, email, password} = req.body;
-    if (!username || !email || !password || username === '' || email === '' || password === ''){
-        next(errorHandler(400, 'All fields are required'))
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("Signup Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All feilds are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
     }
-
-    const hashedPassword = bcrytpjs.hashSync(password, 10)
-
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword
-    });
-
-    try {
-        await newUser.save();
-        res.json("Signup Successfully")
+    const validPassword = bcrytpjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid Password"));
     }
-    catch (error) {
-        next(error);
-    }
-
-}
+    const token = jwt.sign({
+        Id: validUser._id}, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        })
+        .json(rest)
+  } catch (error) {
+    next(error);
+  }
+};
